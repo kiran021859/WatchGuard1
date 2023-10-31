ea5c3a97972696d64b7c384a99577b7aa45a06fc
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-10-31 14:45:48 +0200

    routing to page files

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,10 @@
+
 import './App.css'
+import Communities from './pages/communities page/Communities'
+import SignUp from './pages/sign up page/SignUp'
+import Login from './pages/login page/Login'
 import Layout from './components/Layout/Layout';
+import Landing from './pages/landing page/Landing';
 import Post_page from './pages/post page/post_page';
 import {Routes, Route} from 'react-router-dom';
 
@@ -11,15 +16,15 @@ function App() {
   return (
     <>
     <Routes>
-    <Route index element={<div>landing</div>} />
-    <Route path={'/login'} element={<div>login</div>} />
-    <Route path={'/sign_up'} element={<div>sign_up</div>} />
+    <Route index element={<Landing/>} />
+    <Route path={'/login'} element={<Login/>} />
+    <Route path={'/sign_up'} element={<SignUp/>} />
       <Route path='/user_Dashboard' element={<Layout/>}>
         <Route path={'/user_Dashboard/post'} element={<main><Post_page/></main>} />
-        <Route path={'/user_Dashboard/communities'} element={<div>communities</div>} />
-        <Route path={'/user_Dashboard/feedback'} element={<div>feedback</div>} />
-        <Route path={'/user_Dashboard/privacy'} element={<div>privacy</div>} />
-        <Route path={'/user_Dashboard/safety_tip'} element={<div>safety_tip</div>} />
+        <Route path={'/user_Dashboard/communities'} element={<Communities/>} />
+        <Route path={'/user_Dashboard/feedback'} element={<Feedback/>} />
+        <Route path={'/user_Dashboard/privacy'} element={<Privacy/>} />
+        <Route path={'/user_Dashboard/safety_tip'} element={<Safety/>} />
         <Route path={'/user_Dashboard/help'} element={<div>help</div>} />
         <Route path={'/user_Dashboard/profile'} element={<div>Profile</div>} />
       </Route>
---
+++ front/src/pages/landing page/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>Landing1</div>
+  )
+}
+
+export default Landing
