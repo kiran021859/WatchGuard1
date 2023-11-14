dbf7edc978f79bbac79796c05f3cf949476dd657
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-11-14 10:58:16 +0200

    changed global route to hosted api

--- front/src/context/userContext.jsx
+++ front/src/context/userContext.jsx
@@ -5,7 +5,7 @@ export const UserContext = createContext({});
 export function UserContextProvider({ children }) {
   const [userInfo, setUserInfo] = useState({});
 
-  const http = String('http://localhost:4000')
+  const http = String('https://sore-pear-goose-wig.cyclic.app')
 
   return (
     <UserContext.Provider value={{ userInfo, setUserInfo, http}}>
