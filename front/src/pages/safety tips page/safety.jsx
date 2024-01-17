6c40ba62aa2d6f9c52baacf8a3b21526c0d447db
Kiran Isaacs <kiranisaacs12@gmail.com>
2024-01-17 10:50:30 +0200

    saftey tips page done

--- front/src/pages/safety tips page/safety.jsx
+++ front/src/pages/safety tips page/safety.jsx
@@ -12,28 +12,28 @@ function safety() {
     <div><h1>Safety tips</h1></div>
     <div id='' className='h-[100%] w-full grid grid-cols-safetyBlocks '>
 
-      <div id='' className='flex justify-center flex-col  bg-[green] '>
+      <div id='' className='flex justify-center flex-col items-center'>
         <div id='' className='h-[300px] w-[300px] bg-center bg-cover' style={{backgroundImage: `url(${image2})`}}></div>
-        <h2>LOCK THE DOORS AND THE WIDOWS</h2>
-        <p>Ensure the security of your home by consistently locking doors and windows, even for short absences. Consider the use of deadbolt locks for enhanced security.</p>
+        <h2 id=' ' className=' pt-[5px] pb-[5px] text-[#54DDFC] text-[20px] font-[700] tracking-[1px]'>LOCK THE DOORS AND THE WIDOWS</h2>
+        <p id='' className='text-center text-[18px] font-[400] tracking-[0.9px]'>Ensure the security of your home by consistently locking doors and windows, even for short absences. Consider the use of deadbolt locks for enhanced security.</p>
       </div>
 
-      <div id='' className='flex justify-center  bg-[yellow]'>
+      <div id='' className='flex justify-center flex-col items-center'>
         <div id='' className='h-[300px] w-[300px] bg-center bg-cover' style={{backgroundImage: `url(${image3})`}}></div>
-        <h2>SECURITY SYSTEM</h2>
-        <p>Boost your home's safety by contemplating the installation of a security system equipped with alarms and monitoring services. Visible security signs and stickers can serve as effective deterrents.</p>
+        <h2  id=' ' className=' pt-[5px] pb-[5px] text-[#54DDFC] text-[20px] font-[700] tracking-[1px]' >SECURITY SYSTEM</h2>
+        <p id='' className='text-center text-[18px] font-[400] tracking-[0.9px]'>Boost your home's safety by contemplating the installation of a security system equipped with alarms and monitoring services. Visible security signs and stickers can serve as effective deterrents.</p>
       </div>
 
-      <div id='' className='flex justify-center  bg-[blue]'>
+      <div id='' className='flex justify-center flex-col items-center'>
         <div id='' className='h-[300px] w-[300px] bg-center bg-cover' style={{backgroundImage: `url(${image4})`}}></div>
-        <h2>OUTDOOR LIGHTING</h2>
-        <p>Deter trespassers by implementing proper outdoor lighting. Utilize motion-activated lights to illuminate dark areas and enhance the overall visibility around your property.</p>
+        <h2 id=' ' className=' pt-[5px] pb-[5px] text-[#54DDFC] text-[20px] font-[700] tracking-[1px]'>OUTDOOR LIGHTING</h2>
+        <p  id='' className='text-center text-[18px] font-[400] tracking-[0.9px]'>Deter trespassers by implementing proper outdoor lighting. Utilize motion-activated lights to illuminate dark areas and enhance the overall visibility around your property.</p>
       </div>
 
-      <div id='' className='flex justify-center  bg-[red]'>
+      <div id='' className='flex justify-center flex-col items-center'>
         <div id='' className='h-[300px] w-[300px] bg-center bg-cover' style={{backgroundImage: `url(${image5})`}}></div>
-        <h2>VALUABLES</h2>
-        <p> Safeguard your valuables by keeping them out of sight from windows. Consider investing in a small home safe to secure important documents and possessions, adding an extra layer of protection.</p>
+        <h2 id=' ' className=' pt-[5px] pb-[5px] text-[#54DDFC] text-[20px] font-[700] tracking-[1px]'>VALUABLES</h2>
+        <p id='' className='text-center text-[18px] font-[400] tracking-[0.9px]'> Safeguard your valuables by keeping them out of sight from windows. Consider investing in a small home safe to secure important documents and possessions, adding an extra layer of protection.</p>
       </div>
     </div>
     </>
