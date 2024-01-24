5b011d804dba17088ed535bb22077cc4595628e2
Kiran Isaacs <kiranisaacs12@gmail.com>
2024-01-24 13:36:17 +0200

    comment styleing (not complete)

--- front/src/components/commentBlock/CommentBlock.jsx
+++ front/src/components/commentBlock/CommentBlock.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import pic from '../../assets/pictures/Sorvete_oreo.png'
 
 function CommentBlock({Comment}) {
   return (
-    <div id='' className='bg-[green] grid grid-cols-comment'>
-        <div id='' className='bg-[blue]'><h1>pic</h1></div>
-        <div id='' className='bg-[yellow]'>
-            <p>{Comment}</p>
+    <div id='' className=' grid grid-cols-comment '>
+        <div id='' className='bg-center bg-cover' style={{backgroundImage: `url(${pic})`}}></div>
+        <div id='' className=' '>
+            <h1> user name</h1>
+            <p id='post_text'>{Comment}</p>
         </div>
     </div>
   )
