059b5263d2c2f45f5f3452e9bf640e32ecd78450
Kiran Isaacs <kiranisaacs12@gmail.com>
2024-01-26 10:28:16 +0200

    landing page style

--- front/src/components/landing navbar/navbar.jsx
+++ front/src/components/landing navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, } from 'react';
-//import './navbar.css';
+import './navbar.css';
 import menu from './menu.jpg';
 //import open_menu from './navbar1.js';
 import {Link} from 'react-router-dom'
@@ -17,11 +17,27 @@ function Navbar() {
   return (
     <>
   
-      <div id='' className='bg-[yellow] h-screen grid grid-cols-navbar'>
+      <div id='' className=' bg-white h-screen grid grid-cols-navbar'>
         <nav>
-          <div id='' className="">
-            <Link to='/sign_up'><div id='link'>Sign Up</div></Link>
-            <Link to='/login'><div id='link'>Login</div></Link>
+
+          <div className=' flex justify-center items-center' id='nav_heading'>
+            <h1><span>W</span>atchGuard</h1>
+          </div>
+            
+          <div id='' className='text-center text-[25px] mt-[10%]'>
+            <p>
+              Empower your community with [Your App Name], 
+              a cutting-edge application designed to enhance 
+              safety, security, and connectivity in your 
+              neighborhood. Our app serves as a digital hub 
+              where residents come together to ensure the 
+              well-being of the community.
+            </p>
+          </div>
+
+          <div id='' className=' h-[25vh] flex flex-col items-center justify-between mt-[15%]'>
+            <Link to='/sign_up'><div id='link' className='bg-boring-blue w-[150px] h-[50px] rounded-[5px] text-[20px] font-[500] flex items-center justify-center'>Sign Up</div></Link>
+            <Link to='/login'><div id='link' className='bg-boring-blue w-[150px] h-[50px] rounded-[5px] text-[20px] font-[500] flex items-center justify-center'>Login</div></Link>
           </div>
         </nav>
 
